41b56eeca740de698ec81d93f06c9e1cbb986529
BANTY KUMAR <251283853+banty1288@users.noreply.github.com>
2026-06-21 23:28:48 +0530

    Update BERT.py

--- BERT.py
+++ BERT.py
@@ -1,16 +1,16 @@
-# from transformers import BertTokenizer
-# from transformers import BertModel
-# tokenizer=BertTokenizer.from_pretrained(
-#     "bert-base-uncased"
-# )
-# model=BertModel.from_pretrained(
-#     "bert-base-uncased"
-# )
-# text="I love machine learning"
-# tokens=tokenizer.tokenize(text)
-# print(tokens)
-# encoded=tokenizer.encode(text)
-# print(encoded)
+from transformers import BertTokenizer
+from transformers import BertModel
+tokenizer=BertTokenizer.from_pretrained(
+    "bert-base-uncased"
+)
+model=BertModel.from_pretrained(
+    "bert-base-uncased"
+)
+text="I love machine learning"
+tokens=tokenizer.tokenize(text)
+print(tokens)
+encoded=tokenizer.encode(text)
+print(encoded)
 from transformers import BertTokenizer
 from transformers import BertModel
 import torch
