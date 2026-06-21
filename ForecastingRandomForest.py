1f9842159645177e6dc20c8cb81200784d2ed907
BANTY KUMAR <251283853+banty1288@users.noreply.github.com>
2026-06-21 23:28:19 +0530

    Update ForecastingRandomForest.py

--- ForecastingRandomForest.py
+++ ForecastingRandomForest.py
@@ -1,34 +1,34 @@
-# import pandas as pd
-# values=[10,15,12,18,20,25,22,28,30,35]
-# df=pd.DataFrame({"Value":values})
-# df["Lag_1"]=df["Value"].shift(1)
-# df["Lag_2"]=df["Value"].shift(2)
-# df["Rolling_Mean_3"]=df["Value"].rolling(3).mean()
-# df=df.dropna()
-# X=df[["Lag_1","Lag_2","Rolling_Mean_3"]]
-# Y=df["Value"]
-# split=int(len(df)*0.8)
-# X_train=X[:split]
-# X_test=X[split:]
-# Y_train=Y[:split]
-# Y_test=Y[split:]
-# print(X_train)
-# print(X_test)
-# print(Y_train)
-# print(Y_test)
-# from sklearn.ensemble import RandomForestRegressor
-# model=RandomForestRegressor(
-#     n_estimators=100,
-#     random_state=42
-# )
-# model.fit(X_train,Y_train)
-# prediction=model.predict(X_test)
-# print(prediction)
-# from sklearn.metrics import mean_absolute_error
-# mae=mean_absolute_error(Y_test,prediction)
-# print(mae)
-# importance=pd.DataFrame({
-#     "Feature":X.columns,
-#     "Importance":model.feature_importances_
-# })
-# print(importance)
+import pandas as pd
+values=[10,15,12,18,20,25,22,28,30,35]
+df=pd.DataFrame({"Value":values})
+df["Lag_1"]=df["Value"].shift(1)
+df["Lag_2"]=df["Value"].shift(2)
+df["Rolling_Mean_3"]=df["Value"].rolling(3).mean()
+df=df.dropna()
+X=df[["Lag_1","Lag_2","Rolling_Mean_3"]]
+Y=df["Value"]
+split=int(len(df)*0.8)
+X_train=X[:split]
+X_test=X[split:]
+Y_train=Y[:split]
+Y_test=Y[split:]
+print(X_train)
+print(X_test)
+print(Y_train)
+print(Y_test)
+from sklearn.ensemble import RandomForestRegressor
+model=RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+model.fit(X_train,Y_train)
+prediction=model.predict(X_test)
+print(prediction)
+from sklearn.metrics import mean_absolute_error
+mae=mean_absolute_error(Y_test,prediction)
+print(mae)
+importance=pd.DataFrame({
+    "Feature":X.columns,
+    "Importance":model.feature_importances_
+})
+print(importance)
